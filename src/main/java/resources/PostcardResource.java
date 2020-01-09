3c4ac35664cb0d443a3391707fcc540b8384a190
karinacherkashina <karinacherkashina@192.168.8.105>
2020-01-09 21:10:40 +0200

    add resources. support inner links.

--- src/main/java/resources/PostcardResource.java
+++ src/main/java/resources/PostcardResource.java
@@ -196,6 +196,7 @@ public class PostcardResource
                 if (data.length > 9)
                 {
                     String descr = data[9];
+                    descr = descr.replaceAll("\\[([0-9]{8}[A-Z]{2}[0-9]?)\\|(\\p{L}+)\\]", "<a href=\"/postcard/$1\">$2</a>");
                     descr = descr.replaceAll("\\[([a-zA-Z\\.\\s]+)\\|([a-zA-Z\\s:/\\.]+)\\]", "<a href=\"$2\" target=\"_blank\">$1</a>");
                     postcard.setDescription(descr);
                 }
