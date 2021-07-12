3070b41cb9be2d779d6236de780211bd72c20190
karinacherkashina <karinacherkashina@192.168.8.105>
2021-07-12 17:49:14 +0300

    improve test readability

--- src/test/java/resources/PostcardResourceTest.java
+++ src/test/java/resources/PostcardResourceTest.java
@@ -21,15 +21,15 @@ public class PostcardResourceTest
                 () -> assertEquals(1, postcards.get(14).getSenders().size()),
                 () -> assertEquals("JanaFrank", postcards.get(14).getSenders().get(0)),
                 () -> assertEquals(2, postcards.get(14).getImages().size()),
-                () -> assertEquals("2021_06_21 DE Feed the cat 1.jpg",
+                () -> assertEquals("ImageName 1.jpg",
                         postcards.get(14).getImages().get(0)),
-                () -> assertEquals("2021_06_21 DE Feed the cat 2.jpg",
+                () -> assertEquals("ImageName 2.jpg",
                         postcards.get(14).getImages().get(1)),
 
                 () -> assertEquals("20210129DE", postcards.get(0).getId()),
                 () -> assertEquals(1, postcards.get(0).getTags().size()),
                 () -> assertEquals("project", postcards.get(0).getTags().get(0)),
-                () -> assertEquals("Первая открытка мне как <a href=\"https://www.patreon.com/mammamiu/posts\" target=\"_blank\">патреону Яны Франк</a>|Открытка пришла в авторском конверте. Второе фото лицевая сторона, третье - оборот.",
+                () -> assertEquals("test1 <a href=\"https://www.someUrl.com/\" target=\"_blank\">link title</a>|continue text.",
                         postcards.get(0).getDescription())
         );
     }
