f16a813c233e03e33dddafa372954eb3d61003b9
karinacherkashina <karinacherkashina@192.168.8.105>
2023-10-13 11:59:21 +0200

    Fix path in javaScript

--- src/main/webapp/js/js.js
+++ src/main/webapp/js/js.js
@@ -5,14 +5,14 @@ $(document).ready(function () {
         prevIndex = parseInt(currentIndex) - 1;
         nextIndex = parseInt(currentIndex) + 1;
         
-        $('.imagepreview').css('background-image', 'url("../image/lg/' + imagesList[parseInt(currentIndex)] + '")');
+        $('.imagepreview').css('background-image', 'url("../../image/lg/' + imagesList[parseInt(currentIndex)] + '")');
         
         if (currentIndex === '0') {
             $('#modal-body-prevImg').hide();
         }
         else {
             $('#modal-body-prevImg').show();
-            $('#modal-body-prevImg').attr('src', '../image/100/' + imagesList[prevIndex]);
+            $('#modal-body-prevImg').attr('src', '../../image/100/' + imagesList[prevIndex]);
             $('#modal-body-prevImg').attr('current-index', prevIndex);
         }
         if (imagesList[nextIndex] === '') {
@@ -20,7 +20,7 @@ $(document).ready(function () {
         }
         else {
             $('#modal-body-nextImg').show();
-            $('#modal-body-nextImg').attr('src', '../image/100/' + imagesList[nextIndex]);
+            $('#modal-body-nextImg').attr('src', '../../image/100/' + imagesList[nextIndex]);
             $('#modal-body-nextImg').attr('current-index', nextIndex);
         }
         
